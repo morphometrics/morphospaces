e1acc65d110a3b1f0c8045e9072dc005b785800e
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2024-02-23 13:51:53 +0100

    improve test embeddings

--- src/morphospaces/losses/tests/test_embedding.py
+++ src/morphospaces/losses/tests/test_embedding.py
@@ -5,12 +5,13 @@ from morphospaces.losses.embedding import MultiPosConLoss
 
 def test_multi_pos_con_loss():
     labels = torch.tensor([0, 0, 2, 2])
+    # two classes are clustered and separated
     good_embedding = torch.tensor(
         [
             [0.2, 0.2, 0.2],
             [0.22, 0.21, 0.23],
-            [0.8, 0.8, 0.8],
-            [0.82, 0.81, 0.83],
+            [0.8, 0.1, 0.3],
+            [0.82, 0.11, 0.33],
         ]
     )
 
@@ -18,6 +19,7 @@ def test_multi_pos_con_loss():
 
     good_loss = loss_fn({"feats": good_embedding, "labels": labels})
 
+    # classes are similar, so the loss should be high
     bad_embedding = torch.tensor(
         [
             [0.2, 0.2, 0.2],
