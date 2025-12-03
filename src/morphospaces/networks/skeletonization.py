cb3ee429784b7152e1e63b055cad20f961f1fb86
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2025-12-03 16:29:16 +0100

    add skeleton recall loss

--- src/morphospaces/networks/skeletonization.py
+++ src/morphospaces/networks/skeletonization.py
@@ -2,14 +2,390 @@ from typing import Dict, Tuple
 
 import pytorch_lightning as pl
 import torch
+from monai.data import decollate_batch
+from monai.inferers import sliding_window_inference
+from monai.losses import DeepSupervisionLoss, DiceCELoss
 from monai.metrics import DiceMetric
 from monai.networks.layers import Norm
+from monai.networks.nets import DynUNet
 from monai.networks.nets import UNet as MonaiUnet
 from monai.networks.utils import one_hot
+from monai.transforms import AsDiscrete
 from torch.nn import CrossEntropyLoss
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from morphospaces.logging.image import log_images
 from morphospaces.losses.regression import MaskedSmoothL1Loss
+from morphospaces.losses.util import MaskedDeepSupervisionLoss
+
+
+class SkeletonizationDynUNet(pl.LightningModule):
+    def __init__(
+        self,
+        image_key: str = "raw",
+        labels_key: str = "label",
+        in_channels: int = 1,
+        out_channels: int = 2,
+        kernel_size: tuple[int] = (3, 3, 3, 3, 3, 3),
+        strides: tuple[int] = (1, 2, 2, 2, 2, 2),
+        upsample_kernel_size: tuple[int] = (2, 2, 2, 2, 2),
+        dropout_rate: float = 0.0,
+        learning_rate: float = 1e-4,
+        lr_scheduler_interval: str = "step",
+        lr_scheduler_step: int = 1000,
+        lr_reduction_factor: float = 0.2,
+        lr_reduction_patience: int = 15,
+        log_image_every_n: int = 200,
+        distributed_training: bool = False,
+    ):
+        super().__init__()
+
+        # store parameters
+        self.save_hyperparameters()
+
+        # make the model
+        deep_supr_num = max(1, len(strides) - 2)
+        model = DynUNet(
+            spatial_dims=3,
+            kernel_size=kernel_size,
+            strides=strides,
+            upsample_kernel_size=upsample_kernel_size,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            deep_supervision=True,
+            deep_supr_num=deep_supr_num,
+            dropout=dropout_rate,
+        )
+
+        self._model = model
+
+        # post-processing transforms for validation
+        self.post_pred = AsDiscrete(argmax=True, to_onehot=out_channels)
+        self.post_label = AsDiscrete(to_onehot=out_channels)
+
+        # metrics for training and validation
+        self.loss_function = DeepSupervisionLoss(
+            DiceCELoss(to_onehot_y=True, softmax=True)
+        )
+        self.validation_metric = DiceMetric(
+            include_background=False, reduction="mean", get_not_nans=False
+        )
+
+        # parameters to track training
+        self.iteration_count = 0
+        self.validation_step_outputs = []
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Inference forward pass."""
+        return self._model(x)
+
+    def configure_optimizers(self):
+        """Set up the Adam optimzier.
+        See the pytorch-lightning module documentation for details.
+        """
+        optimizer = torch.optim.AdamW(
+            self._model.parameters(), self.hparams.learning_rate
+        )
+        learning_rate_scheduler = ReduceLROnPlateau(
+            optimizer=optimizer,
+            factor=self.hparams.lr_reduction_factor,
+            patience=self.hparams.lr_reduction_patience,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": learning_rate_scheduler,
+                "interval": self.hparams.lr_scheduler_interval,
+                "frequency": self.hparams.lr_scheduler_step,
+                "monitor": "val_loss",
+            },
+        }
+
+    def training_step(
+        self, batch: Dict[str, torch.Tensor], batch_idx: int
+    ) -> Dict[str, float]:
+        """Implementation of one training step.
+        See the pytorch-lightning module documentation for details.
+        """
+        images = batch[self.hparams.image_key]
+        labels = batch[self.hparams.labels_key]
+
+        # make the forward pass and compute the loss
+        logits = self.forward(images)
+
+        # for the deep supervision make each scale an element of the list
+        logits = torch.unbind(logits, dim=1)
+        loss = self.loss_function(logits, labels)
+
+        # log the loss and learning rate
+        self.log(
+            "training_loss",
+            loss,
+            batch_size=len(images),
+            prog_bar=True,
+            sync_dist=self.hparams.distributed_training,
+        )
+
+        # log the images
+        if (self.iteration_count % self.hparams.log_image_every_n) == 0:
+            # only log every 200 iterations
+            log_images(
+                input=images,
+                target=labels,
+                prediction=logits,
+                iteration_index=self.iteration_count,
+                logger=self.logger.experiment,
+            )
+
+        self.iteration_count += 1
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        """Implementation of one validation step.
+        This performs inference on the entire validation image
+        using a sliding window. See the pytorch-lightning
+        module documentation for details.
+        """
+        # get the images
+        images = batch[self.hparams.image_key]
+        labels = batch[self.hparams.labels_key]
+
+        # infer on the whole patch with sliding window
+        roi_size = (96, 96, 96)
+        sw_batch_size = 4
+        outputs = sliding_window_inference(
+            images, roi_size, sw_batch_size, self.forward
+        )
+
+        # compute the loss
+        loss = self.loss_function(outputs, labels)
+
+        # store the val loss
+        val_outputs = {
+            "val_loss": loss,
+            "val_number": len(images),
+        }
+        self.validation_step_outputs.append(val_outputs)
+
+        # compute the val metric
+        outputs = [self.post_pred(i) for i in decollate_batch(outputs)]
+        labels = [self.post_label(i) for i in decollate_batch(labels)]
+        self.validation_metric(y_pred=outputs, y=labels)
+
+        return val_outputs
+
+    def on_validation_epoch_end(self):
+        """Implementation of the validation epoch end.
+
+        This computes the mean validation loss across all
+        validation steps.
+
+        See the pytorch-lightning module documentation for details.
+        """
+        # compute the mean validation loss
+        val_loss, num_items = 0, 0
+        for output in self.validation_step_outputs:
+            val_loss += output["val_loss"].sum().item()
+            num_items += output["val_number"]
+        mean_val_loss = torch.tensor(val_loss / num_items)
+        self.log(
+            "val_loss",
+            mean_val_loss,
+            batch_size=num_items,
+            sync_dist=self.hparams.distributed_training,
+        )
+        self.validation_step_outputs.clear()  # free memory
+
+        # compute the mean validation metric
+        mean_val_dice = self.validation_metric.aggregate().item()
+        self.validation_metric.reset()
+        self.log(
+            "val_dice",
+            mean_val_dice,
+            batch_size=num_items,
+            sync_dist=self.hparams.distributed_training,
+        )
+
+        return {"val_loss": mean_val_loss, "val_dice": mean_val_dice}
+
+
+class SkeletonizationRegressionDynUNet(pl.LightningModule):
+    def __init__(
+        self,
+        image_key: str = "raw",
+        labels_key: str = "label",
+        segmentation_key: str = "segmentation",
+        in_channels: int = 1,
+        out_channels: int = 2,
+        kernel_size: tuple[int] = (3, 3, 3, 3, 3, 3),
+        strides: tuple[int] = (1, 2, 2, 2, 2, 2),
+        upsample_kernel_size: tuple[int] = (2, 2, 2, 2, 2),
+        dropout_rate: float = 0.0,
+        learning_rate: float = 1e-4,
+        lr_scheduler_interval: str = "step",
+        lr_scheduler_step: int = 1000,
+        lr_reduction_factor: float = 0.2,
+        lr_reduction_patience: int = 15,
+        log_image_every_n: int = 200,
+        distributed_training: bool = False,
+    ):
+        super().__init__()
+
+        # store parameters
+        self.save_hyperparameters()
+
+        # make the model
+        deep_supr_num = max(1, len(strides) - 2)
+        model = DynUNet(
+            spatial_dims=3,
+            kernel_size=kernel_size,
+            strides=strides,
+            upsample_kernel_size=upsample_kernel_size,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            deep_supervision=True,
+            deep_supr_num=deep_supr_num,
+            dropout=dropout_rate,
+        )
+
+        self._model = model
+
+        # post-processing transforms for validation
+        self.post_pred = AsDiscrete(argmax=True, to_onehot=out_channels)
+        self.post_label = AsDiscrete(to_onehot=out_channels)
+
+        # metrics for training and validation
+        self.loss_function = MaskedDeepSupervisionLoss(
+            MaskedSmoothL1Loss(reduction="mean")
+        )
+
+        # parameters to track training
+        self.iteration_count = 0
+        self.validation_step_outputs = []
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Inference forward pass."""
+        return self._model(x)
+
+    def configure_optimizers(self):
+        """Set up the Adam optimzier.
+        See the pytorch-lightning module documentation for details.
+        """
+        optimizer = torch.optim.AdamW(
+            self._model.parameters(), self.hparams.learning_rate
+        )
+        learning_rate_scheduler = ReduceLROnPlateau(
+            optimizer=optimizer,
+            factor=self.hparams.lr_reduction_factor,
+            patience=self.hparams.lr_reduction_patience,
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": learning_rate_scheduler,
+                "interval": self.hparams.lr_scheduler_interval,
+                "frequency": self.hparams.lr_scheduler_step,
+                "monitor": "val_loss",
+            },
+        }
+
+    def training_step(
+        self, batch: Dict[str, torch.Tensor], batch_idx: int
+    ) -> Dict[str, float]:
+        """Implementation of one training step.
+        See the pytorch-lightning module documentation for details.
+        """
+        images = batch[self.hparams.image_key]
+        labels = batch[self.hparams.labels_key]
+        segmentation = batch[self.hparams.segmentation_key]
+
+        # make the forward pass and compute the loss
+        prediction = self.forward(images)
+
+        # for the deep supervision make each scale an element of the list
+        prediction = torch.unbind(prediction, dim=1)
+        loss = self.loss_function(
+            input=prediction, target=labels, mask=segmentation
+        )
+
+        # log the loss and learning rate
+        self.log(
+            "training_loss",
+            loss,
+            batch_size=len(images),
+            prog_bar=True,
+            sync_dist=self.hparams.distributed_training,
+        )
+
+        # log the images
+        if (self.iteration_count % self.hparams.log_image_every_n) == 0:
+            # only log every 200 iterations
+            log_images(
+                input=images,
+                target=labels,
+                prediction=prediction,
+                iteration_index=self.iteration_count,
+                logger=self.logger.experiment,
+            )
+
+        self.iteration_count += 1
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        """Implementation of one validation step.
+        This performs inference on the entire validation image
+        using a sliding window. See the pytorch-lightning
+        module documentation for details.
+        """
+        # get the images
+        images = batch[self.hparams.image_key]
+        labels = batch[self.hparams.labels_key]
+        segmentation = batch[self.hparams.segmentation_key]
+
+        # infer on the whole patch with sliding window
+        roi_size = (96, 96, 96)
+        sw_batch_size = 4
+        outputs = sliding_window_inference(
+            images, roi_size, sw_batch_size, self.forward
+        )
+
+        # compute the loss
+        loss = self.loss_function(
+            input=outputs, target=labels, mask=segmentation
+        )
+
+        # store the val loss
+        val_outputs = {
+            "val_loss": loss,
+            "val_number": len(images),
+        }
+        self.validation_step_outputs.append(val_outputs)
+
+        return val_outputs
+
+    def on_validation_epoch_end(self):
+        """Implementation of the validation epoch end.
+
+        This computes the mean validation loss across all
+        validation steps.
+
+        See the pytorch-lightning module documentation for details.
+        """
+        # compute the mean validation loss
+        val_loss, num_items = 0, 0
+        for output in self.validation_step_outputs:
+            val_loss += output["val_loss"].sum().item()
+            num_items += output["val_number"]
+        mean_val_loss = torch.tensor(val_loss / num_items)
+        self.log(
+            "val_loss",
+            mean_val_loss,
+            batch_size=num_items,
+            sync_dist=self.hparams.distributed_training,
+        )
+        self.validation_step_outputs.clear()  # free memory
+
+        return {"val_loss": mean_val_loss}
 
 
 class SkeletonizationNet(pl.LightningModule):
